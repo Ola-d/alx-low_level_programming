360832c070f6ed7f6abed708afdb3052eb644095
ola-d <onioladimeji19@gmail.com>
2022-03-13 05:33:09 -0700

    lingual

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
-# include <stduo.h>
-/**
- *
- * Return: 0 after printing the functuon 
- */
-int main (void)
-{
-	puts("\"Programming is like building a multilingual puzzle);
-	return (0);
-}
+include <stdio.h>
 
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
 
