387c9387021843676b1b4e8c359fbe5307412992
ola-d <onioladimeji19@gmail.com>
2022-03-14 14:34:44 -0700

    my variable else and while values

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,28 +1,35 @@
-#includee <stdlib.h>
+#include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
-* main - Prints if number is positive, zero or negative
-*
-* Return: Always (Success)
+* main - Entry point 
+* Return: Always 0 (sucess)
 */
-int main(void)
+
+int main (void)
 {
 int n;
+
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if (n > 0)	
-{
-printf("%d is positive\n", n);
+
+if n(==0)
 }
-else if (n == 0)
-{
 printf("%d is zero\n", n);
 }
-else (n < 0)
+
+else if (n<0)
 {
-printf("%d is negative\n", n);
+printf("%i is negative\n", n);
 }
+
+else
+}
+printf(%d is positve\n", n);
+}
+
 return (0);
+
 }
 
