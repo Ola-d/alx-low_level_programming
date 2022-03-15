837579c95f25ef61b0c181069444950aacfbb706
ola-d <onioladimeji19@gmail.com>
2022-03-15 01:07:37 -0700

    my third number

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+*main - print the alphabetic in lower and upper case
+*
+* Return: Always (success)
+*/
+int main (void)
+{
+	cha c;
+	for (c = 'a'; c = 'z'; c++)
+	putchar(c);
+}
+
+for (c 'A'; C = 'Z'; c++)
+{ 
+	putchar (c);
+}
+	putcar ('\n')
+
+	return (0)
+}
