474673ad41b331a11de216895cc7417d9fcd1ef3
ola-d <onioladimeji19@gmail.com>
2022-03-15 15:32:18 -0700

    my new variables

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,18 +10,17 @@ int main (void)
 	char a = 'a';
 	char A = 'A'
 	
-	while (a <= 'z')
-	{
-		putchar(a);
-		a++;
-	}
-	while (A <= 'A')
-	{
-		putchar(A);
-		A++;
-	}
-	putchar('\n');
-
-	return (0);
+while (a <= 'z')
+{
+putchar(a);
+a++;
+}
+while (A <= 'A')
+{
+putchar(A);
+A++;
+}
+putchar('\n');
+return (0);
 }
 
