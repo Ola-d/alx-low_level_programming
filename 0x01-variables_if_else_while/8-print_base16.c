e8ff24c8c2a899322372f9addb26e312dc5073cc
ola-d <onioladimeji19@gmail.com>
2022-03-16 03:08:32 -0700

    my variable number

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,13 +11,12 @@ int main(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		putchar(c);
+	putchar(c);
 	}
 	for (c = 'a'; c <= 'f'; c++)
 	{
-		putchar(c);		
+	putchar(c);		
 	}
-	
 	putchar('\n');
 	
 	return (0);
