5b2b5013f96fd3e4bafd006f27315a0148e0d4d1
ola-d <onioladimeji19@gmail.com>
2022-03-16 00:16:45 -0700

    my variables value

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - print some letters alphabet
+ *
+ * Return : Always (success)
+ */
+int main (void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z'; c++)
+	{
+	    if (c != 'e' && c != 'q')
+	    {
+		purchar(c);
+	    }
+	}
+
+	purchar('\n');
+
+	return (0);
+}
