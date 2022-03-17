79e9d60e8b39b5c52d45027e4cefcaa451416ff2
ola-d <onioladimeji19@gmail.com>
2022-03-17 01:25:40 -0700

    my positive numbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,14 +5,14 @@
 /**
  * main - Prints if number is positive, zero or negative
  *
- * Return: Always (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
-	n = rand( ) - RAND_MAX / 2;
+	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
 	printf("%d is positive\n", n);
